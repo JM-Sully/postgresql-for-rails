abb4d2a5d739c4cab92937d6fccdd6ea24443b98
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2026-01-14 09:56:26 +0000

    Correct Rubocop issue with Lint/AssignmentInCondition
    Use == if you meant to do a comparison or wrap the expression in parentheses to indicate you meant to assign in a condition.

--- app/services/trip_search.rb
+++ app/services/trip_search.rb
@@ -8,7 +8,7 @@ class TripSearch
   end
 
   def start_location
-    if text = params[:start_location]
+    if (text = params[:start_location])
       Trip.with_start_location(sanitize(text))
     else
       Trip.all
@@ -16,7 +16,7 @@ class TripSearch
   end
 
   def driver_name
-    if text = params[:driver_name]
+    if (text = params[:driver_name])
       Trip.with_driver_name(sanitize(text))
     else
       Trip.all
@@ -24,7 +24,7 @@ class TripSearch
   end
 
   def rider_name
-    if text = params[:rider_name]
+    if (text = params[:rider_name])
       Trip.with_rider_name(sanitize(text))
     else
       Trip.all
